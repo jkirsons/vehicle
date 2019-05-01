b0326529061a975103def3ecea4ab7a410c631fb
Jason <jkirsons@gmail.com>
2019-05-01 15:40:25 +0200

    update

--- Models/fixQuads.py
+++ Models/fixQuads.py
@@ -9,12 +9,17 @@ import bpy
 
 # https://docs.blender.org/api/blender_python_api_2_59_0/bpy.ops.export_scene.html
 
+#~/Documents/GoDot/vehicle/Models/roadTile_001.obj
+
 for obj in glob.glob('*.obj'):
   bpy.ops.import_scene.obj(filepath=obj)
   model = bpy.data.objects[len(bpy.data.objects) - 1]
 
   bpy.context.scene.objects.active = model
 
-  bpy.ops.object.mode_set(mode='EDIT')
-
+#  bpy.ops.object.mode_set(mode='EDIT')
+  
+  bpy.ops.transform.translate(value=(-1.5, -1.5, 0.0))
+  bpy.ops.object.transform_apply(location = True, scale = True, rotation = True)
+  
   bpy.ops.export_scene.obj(filepath=obj, use_selection=True, use_triangles=True)
